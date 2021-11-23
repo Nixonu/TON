18c64e03070e22c9526f389856db47db302b4c9d
Nixonu <nixonu@ya.ru>
2021-11-23 19:24:45 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 dir="/home/user/ton"
-#tonminer="/home/user/ton/tonminer"
-#rm -rf $dir
-#rm $tonminer
-#mkdir -p $dir
-#wget https://newton-blockchain.github.io/global.config.json -qP $dir
-#wget https://raw.githubusercontent.com/bu11ettrain/ton/main/customglobalconfig/global.config.json -qP $dir
-#wget https://github.com/bu11ettrain/ton/raw/main/minertools-cuda-ubuntu-18.04-x86-64.tar.gz -qP $dir
-#wget https://github.com/bu11ettrain/ton/raw/main/minertools-opencl-ubuntu-18.04-x86-64.tar.gz -qP $dir
-#wget https://raw.githubusercontent.com/bu11ettrain/ton/main/screen-kill -qP $dir
-#wget https://raw.githubusercontent.com/bu11ettrain/ton/main/tonminer -qP $dir
-#wget https://raw.githubusercontent.com/Nixonu/TON/main/tonminer -qP $dir
-#wget https://raw.githubusercontent.com/bu11ettrain/ton/main/tonmining -qP $dir
-#tar -xf $dir/minertools-cuda-ubuntu-18.04-x86-64.tar.gz -C $dir
-#tar -xf $dir/minertools-opencl-ubuntu-18.04-x86-64.tar.gz -C $dir
-#chmod +x $dir/ton*
-#chmod +x $dir/screen-kill
+chmod +x $dir/ton*
+chmod +x $dir/screen-kill
 sed -i "s/GIVER/$1/" $dir/tonminer
-#miner stop
+miner stop
 $dir/tonmining
