df41535ecc9fa460a5fa3e4f7ae2d9c0fee56039
Nixonu <nixonu@ya.ru>
2021-11-23 20:40:16 +0500

    Update install9.sh

--- install9.sh
+++ install9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dir="/home/user/ton"
 chmod +x $dir/tonminer
-sed -i "s/kf8gf1PQy4u2kURl-Gz4LbS29eaN4sVdrVQkPO-JL80VhOe6/$1/" $dir/tonminer
+sed -i "s/$1/$2/" $dir/tonminer
 $dir/tonmining
