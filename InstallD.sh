b2844a40d68f5fe36a4bbc2194e2d5ed7f5eeb4d
Nixonu <nixonu@ya.ru>
2021-11-29 14:23:45 +0500

    Update InstallD.sh

--- InstallD.sh
+++ InstallD.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-1.0.4-linux.tar.gz -qp /home/user
 tar -C /home/user -xf danila-miner-1.0.4-linux.tar.gz
+tar -xf /home/user/danila-miner-1.0.4-linux.tar.gz -C /home/user
 wget https://raw.githubusercontent.com/nixonu/ton/main/miner.service -qP /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable miner
