a358f5bbc86313a5fa82012cb60e934e76a37f82
Nixonu <nixonu@ya.ru>
2021-11-29 14:33:40 +0500

    Update installd2.sh

--- installd2.sh
+++ installd2.sh
@@ -1,4 +1,3 @@
-tar -xf /home/user/danila-miner-1.0.4-linux.tar.gz -C /home/user
 wget https://raw.githubusercontent.com/nixonu/ton/main/miner.service -qP /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable miner
